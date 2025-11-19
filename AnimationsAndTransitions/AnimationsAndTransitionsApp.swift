321ba8a959909626f823e3f9fda3798c355826a8
MindaugasBalakauskas <mindaugas.balakauskas@gmail.com>
2025-11-19 15:28:05 +0000

    Initial Commit

---
+++ AnimationsAndTransitions/AnimationsAndTransitionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsAndTransitionsApp.swift
+//  AnimationsAndTransitions
+//
+//  Created by M Balakauskas on 19/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsAndTransitionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
