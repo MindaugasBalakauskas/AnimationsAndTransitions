8b73674d518096bd7d84f6626a1ba86889cdda97
MindaugasBalakauskas <mindaugas.balakauskas@gmail.com>
2025-11-19 16:10:24 +0000

    Button with animation

--- AnimationsAndTransitions/ContentView.swift
+++ AnimationsAndTransitions/ContentView.swift
@@ -8,14 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var buttonTapped = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Button("Click me!") {
+       //     withAnimation {
+                buttonTapped.toggle()
+       //     }
         }
         .padding()
+        .background(buttonTapped ? .blue :.green)
+        .foregroundColor(.white)
+        .cornerRadius(15)
+        .font(.headline)
+        .animation(.default, value: buttonTapped)
+        .scaleEffect(buttonTapped ? 1.3 : 1.0)
+        .animation(.easeInOut(duration: 1), value: buttonTapped)
+        
     }
 }
 
