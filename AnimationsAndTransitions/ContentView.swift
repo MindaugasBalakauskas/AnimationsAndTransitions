a79601f8b93f70dec3af5455228b4daf7f138b55
MindaugasBalakauskas <mindaugas.balakauskas@gmail.com>
2025-11-24 14:30:23 +0000

    Reorganizing code

--- AnimationsAndTransitions/ContentView.swift
+++ AnimationsAndTransitions/ContentView.swift
@@ -15,17 +15,33 @@ struct ScalingButtonStyle: ButtonStyle {
             .foregroundColor(.white)
             .cornerRadius(15)
             .font(.headline)
+      //      .animation(nil, value: configuration.isPressed)
             .scaleEffect(configuration.isPressed ? 1.3 : 1.0)
             .animation(.easeInOut(duration: 0.2), value: configuration.isPressed)
     }
 }
 
 struct ContentView: View {
+    
+    @State private var showImage = false
+    
     var body: some View {
-        Button("Click me!") {
-     
+        VStack {
+            Button(showImage ? "Make it go away!": "Make an image appear!") {
+                withAnimation {
+                    showImage.toggle()
+                }
+               
+            }
+            .buttonStyle(ScalingButtonStyle())
+            
+           
+            Image("pikachu")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 200)
+                    .opacity(showImage ? 1 : 0)
         }
-        .buttonStyle(ScalingButtonStyle())
     }
 }
 
